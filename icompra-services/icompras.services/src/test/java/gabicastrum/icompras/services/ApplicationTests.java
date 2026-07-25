433543a4cf3833b67598b7be9670f170cdcf17e5
Gabriela de Castro Laurindo <gabriela.laurindo@dbserver.com.br>
2026-07-25 16:22:31 -0300

    config: commit inicial

---
+++ icompra-services/icompras.services/src/test/java/gabicastrum/icompras/services/ApplicationTests.java
@@ -0,0 +1,13 @@
+package gabicastrum.icompras.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
